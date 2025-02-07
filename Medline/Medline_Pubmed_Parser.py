21ac0e4946aa5fa275775122fc4a97beccc711c6
Suresh D <38948398+stormsuresh92@users.noreply.github.com>
2025-02-07 16:49:28 +0530

    Update Medline_Pubmed_Parser.py

--- Medline/Medline_Pubmed_Parser.py
+++ Medline/Medline_Pubmed_Parser.py
@@ -2,28 +2,15 @@ from Bio import Medline
 import pandas as pd
 from tqdm import tqdm
 
-
 alldata = []
 with open('pubmed-activating-set.txt', encoding='ISO 8859-7') as data:
     pmids = Medline.parse(data)
     for pmid in tqdm(pmids):
-        try:
-            pi = pmid['PMID']
-        except:
-            pi = ''
-        try:
-            ab = pmid['AB']
-        except:
-            ab = ''
         dic = {
-            'PMID':pi,
-            'abstract':ab
+            'PMID': pmid.get('PMID', ''),
+            'abstract': pmid.get('AB', '')
         }
         alldata.append(dic)
 
 df = pd.DataFrame(alldata)
 df.to_csv('output.csv', index=False)
-print('fin')
-
-
-input()
